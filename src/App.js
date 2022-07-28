b0addc7c7104048ae6cab23410a66fba75ade8fa
Saroj Niraula <sar.niraula162@gmail.com>
2022-07-28 15:28:54 +0545

    Complete ui

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import TopButtons from './components/TopButtons';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+import getWeatherData from './services/WeatherService';
 
 function App() {
+
+  const fetchWeather = async() => {
+    const data = await getWeatherData('weather', {q: 'London'});
+    console.log(data);
+  };
+
+  fetchWeather();
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 from-cyan-700 to-blue-700">
         <TopButtons/>
         <Inputs />
         <TimeAndLocation />
+        <TemperatureAndDetails />
+        <Forecast title="Hourly Forecast"/>
+        <Forecast title="Daily Forecast"/>
+
     </div>
   );
 }
---
+++ src/services/WeatherService.js
@@ -0,0 +1,15 @@
+const API_KEY = 'b25e4be43f354bc866b66345f44eea85'
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + '/' + infoType);
+    url.search = new URLSearchParams({...searchParams, appid: API_KEY})
+
+    return fetch(url)
+        .then((res) => res.json())
+}
+
+const getFormattedWeatherData = (searchParams) => {
+
+} 
